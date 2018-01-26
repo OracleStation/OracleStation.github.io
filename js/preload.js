c090a5aa8f6f66598d20227dd7fc54b522f09fc9
root <root@valkyrie.sexy>
2018-01-26 18:39:27 -0500

    modified:   css/style.css modified:   index.html modified:   js/preload.js

--- js/preload.js
+++ js/preload.js
@@ -1,5 +1,4 @@
-$(window).on('load', function() { // makes sure the whole site is loaded
-  $('#preload-image').fadeOut(); // will first fade out the loading animation
-  $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-  $('body').css({'overflow':'visible'});
-})
+window.onload = function(){
+	var el = document.getElementById('preloader');
+	el.style.display = 'none';
+};
