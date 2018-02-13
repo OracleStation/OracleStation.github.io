3afb353c885581b910ca3b43ebac5e542b5016a1
r3valkyrie <valkyrie@valkyrie.sexy>
2018-02-13 07:55:32 -0500

    adds merge/close icons

--- assets/app.php
+++ assets/app.php
@@ -24,14 +24,21 @@ $data = file_get_contents("https://api.github.com/repos/oraclestation/oraclestat
 function getClosedPRs(){
     $data = file_get_contents('cache/pulls.json', true);
     $jsonArray = json_decode($data, true);
+    $mergeStatus = "";
 
     foreach($jsonArray as $k=> $fuck){
         if($k < 5) {
+            if($jsonArray[$k]['merged_at'] == null){
+                $mergeStatus = 'images/closed.svg';
+            }
+            else {
+                $mergeStatus = 'images/merged.svg';
+            }
             echo("<div class='pull-request'>
 <a href='{$jsonArray[$k]['user']['url']}'><img class='avatar' src='{$jsonArray[$k]['user']['avatar_url']}'></a>
-<a href='{$jsonArray[$k]['html_url']}'><h5>{$jsonArray[$k]['title']}</h5></a>
-<p>{$jsonArray[$k]['body']}</p>
-<p>By {$jsonArray[$k]['user']['login']}</p>
+<a href='{$jsonArray[$k]['html_url']}'><h5><img class='mergestatus' src='{$mergeStatus}'>{$jsonArray[$k]['title']}</h5></a>
+<p>{$jsonArray[$k]['body']}<br>
+By {$jsonArray[$k]['user']['login']}</p>
 </div>
 ");
             $k++;
